d21edc08053198d46e3e53a9a800eb17be638ed4
jaychoo <jaychoo@gmail.com>
2018-10-11 22:56:12 -0400

    add shortcut

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -36,6 +36,7 @@
 (global-set-key [f3] 'helm-projectile-grep)
 (global-set-key [f4] 'helm-projectile)
 (global-set-key [f5] 'shell-pop)
+(global-set-key [f6] 'goto-line)
 (global-set-key [f7] 'toggle-comment-on-line)
 
 (global-set-key (kbd "M-0") 'delete-window)
